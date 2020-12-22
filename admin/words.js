d48ef2a1a45c9f561a819825968fb1b1c68ecece
arteck <arteck@outlook.com>
2020-12-22 08:18:09 +0100

    init

---
+++ admin/words.js
@@ -0,0 +1,103 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+ "active":      {
+     "en": "active",
+     "de": "aktiv",
+     "ru": "активный",
+     "pt": "ativo",
+     "nl": "actief",
+     "fr": "actif",
+     "it": "attivo",
+     "es": "activo",
+     "pl": "aktywny",
+     "zh-cn": "活性"
+ },
+ "addresses":   {
+     "en": "addresses to monitoring",
+     "de": "Adressen zur Überwachung",
+     "ru": "обращается к мониторингу",
+     "pt": "endereços para monitoramento",
+     "nl": "adressen voor monitoring",
+     "fr": "adresses à la surveillance",
+     "it": "indirizzi al monitoraggio",
+     "es": "direcciones para monitorear",
+     "pl": "adresy do monitoringu",
+     "zh-cn": "监控地址"
+ },
+ "interval":    {
+     "en": "Polling interval[sec]",
+     "de": "Abfrageintervall [Sek.]",
+     "ru": "Интервал опроса [сек]",
+     "pt": "Intervalo de sondagem [seg]",
+     "nl": "Polling-interval [sec]",
+     "fr": "Intervalle d'interrogation [sec]",
+     "it": "Intervallo di polling [sec]",
+     "es": "Intervalo de sondeo [seg]",
+     "pl": "Okres odpytywania [s]",
+     "zh-cn": "轮询间隔[秒]"
+ },
+
+"batterynum":    {
+    "en": "installed Batterys",
+    "de": "installierte Batterien",
+    "ru": "установлены батареи",
+    "pt": "Baterias instaladas",
+    "nl": "geïnstalleerde batterijen",
+    "fr": "Batteries installées",
+    "it": "batterie installate",
+    "es": "baterías instaladas",
+    "pl": "zainstalowane Batterys",
+    "zh-cn": "已安装电池"
+},
+"arraynum":    {
+    "en": "installed arrays of Battery",
+    "de": "installierte Arrays",
+    "ru": "установленные массивы Battery",
+    "pt": "matrizes instaladas de bateria",
+    "nl": "geïnstalleerde arrays van batterij",
+    "fr": "baies installées de batterie",
+    "it": "array di batterie installati",
+    "es": "matrices instaladas de batería",
+    "pl": "zainstalowane tablice baterii",
+    "zh-cn": "已安装的电池阵列"
+},
+
+ "ipaddress":   {
+     "en": "IP Address",
+     "de": "IP Adresse",
+     "ru": "Айпи адрес",
+     "pt": "Endereço de IP",
+     "nl": "IP adres",
+     "fr": "Adresse IP",
+     "it": "Indirizzo IP",
+     "es": "Dirección IP",
+     "pl": "Adres IP",
+     "zh-cn": "IP地址"
+ },
+ "name":        {
+     "en": "Name",
+     "de": "Name",
+     "ru": "имя",
+     "pt": "Nome",
+     "nl": "Naam",
+     "fr": "Nom",
+     "it": "Nome",
+     "es": "Nombre",
+     "pl": "Nazwa",
+     "zh-cn": "名称"
+ },
+ "settings":    {
+     "en": "Adapter settings",
+     "de": "Adaptereinstellungen",
+     "ru": "Настройки адаптера",
+     "pt": "Configurações do adaptador",
+     "nl": "Adapter instellingen",
+     "fr": "Paramètres de l'adaptateur",
+     "it": "Impostazioni dell'adattatore",
+     "es": "Configuración del adaptador",
+     "pl": "Ustawienia adaptera",
+     "zh-cn": "适配器设定"
+ },
+};
---
+++ main.js
@@ -0,0 +1,529 @@
+/**
+ *
+ *      ioBroker bydbatt Adapter
+ *
+ *      (c) 2014-2020 arteck <arteck@outlook.com>
+ *
+ *      MIT License
+ *
+ */
+
+'use strict';
+ 
+const utils = require('@iobroker/adapter-core');
+const axios = require('axios');
+
+let _batteryNum = 0;
+let _arrayNum = 0;
+let requestTimeout = null;
+let interval = 0;
+
+class bydbattControll extends utils.Adapter {
+
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: 'bydbatt',
+        });
+        this.on('ready', this.onReady.bind(this));
+        this.on('unload', this.onUnload.bind(this));
+
+    }
+
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    async onReady() {
+        this.setState('info.connection', false, true);
+
+        await this.initialization();
+        await this.create_state();
+        await this.getInfos();
+    }
+
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            if (requestTimeout) clearTimeout(requestTimeout);
+
+            this.log.info('cleaned everything up...');
+            this.setState('info.connection', false, true);
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+
+    async getInfos() {
+        this.log.debug(`get Information`);
+   
+        if (requestTimeout) clearTimeout(requestTimeout);
+
+        for (var a = 0; a < _arrayNum; a++) {
+            for (var b = 0; b < _batteryNum; b++) {
+
+                const htmlData = await this.getDaten(this.config.ip, a, b);
+                const res = await this.updateDevice(htmlData_test, a, b);
+
+
+            }
+        }
+
+        requestTimeout = setTimeout(async () => {
+            this.getInfos();
+        }, interval);
+        
+    }
+
+
+    async getDaten(ip, arrNum, battNum) {
+        const statusURL = `http://user:user@${ip}/asp/RunData.asp`;
+        const res = await axios.get(statusURL);
+        const htmlData = res.data;
+        return htmlData;
+    }
+
+    async updateDevice(htmlData, arrNum, battNum) {
+        const arrNumNow = arrNum +1;
+        const battNumNow = battNum +1;
+        let serialNumPosi = "";
+
+
+        let htmlText2 = (htmlData || '').toString().replace(/\r\n|[\r\n]/g, ' ');
+            htmlText2 = (htmlText2 || '').toString().replace(/\t|[\t]/g, ' ');
+
+        const g1 = /<td(>|[^>]+>)((?:.(?!<\/td>))*.?)<\/td>/g;                       // suche alle td
+        const g2 = /[a-zA-Z ]+:|[a-zA-Z]+(\[(?:\[??[^\[]*?\])):|value=\d*\.?\d*/g;   // suche alle bezeichnungnen und values
+        const g3 = /\w*\d*-\w*\d*/g; // suche serialnummer
+
+        try {
+
+            var contents = htmlText2.match(g1);
+            contents = contents.filter(function(el){
+                if (el.indexOf("SerialNumber") > 1) {
+                    serialNumPosi = el;
+                }
+                return el.indexOf(":") || el.indexOf("value");
+            })
+
+            const balanceCtrl = contents[contents.length-1];
+            const serialNumValue = contents[contents.indexOf(serialNumPosi)+1].match(g3).toString();
+
+            let treffer = contents.toString();
+            var contents = treffer.match(g2);
+
+            contents = contents.filter(function(r1){
+                return r1.indexOf(":") > 0 || r1.length > 6;
+            })
+
+            const stateArray = await this.getObjectViewAsync('system', 'state', {startkey: this.namespace + '.', endkey: this.namespace +  '.BattNum.1' + '\u9999'})
+
+            if (stateArray && stateArray.rows.length > 0) {
+                for (let i = 0; i < stateArray.rows.length; i++) {
+                    if (stateArray.rows[i].id) {
+                        let id = stateArray.rows[i].id;
+
+                        id = id.replace("ArrayNum.1", "ArrayNum."+ arrNumNow).replace("BattNum.1", "BattNum." + battNumNow);  // tausche array auf aktuell
+
+
+                        if (id.indexOf("lastInfoUpdate") > 0) {
+                            this.setState(obje._id, Date.now(), true);
+                        }
+
+                        var idx = 0;
+
+                        for (; idx < contents.length+1;) {
+                            let idCon = contents[idx];
+
+                            if (idCon.indexOf(" ") > 0) {
+                                contents.shift();
+                                continue;
+                            }
+
+                            if (idCon.indexOf("value=") !== 0) {
+                                idCon = idCon.replace(":", "").replace("[", "").replace("]", "");
+
+                                if (id.indexOf(idCon) > 0) {
+                                    let wert = contents[idx + 1];
+                                    if (idCon == "SerialNumber") {
+                                        wert = serialNumValue;   // serialnummer sonderlocke
+                                    } else {
+                                        wert = wert.replace("value=", "");
+                                    }
+                                    this.setState(id, wert, true);
+                                    contents.shift();
+                                    contents.shift();
+                                    break;
+                                }
+                            }
+                            idx++;
+                        }
+                    }
+                }
+            }
+        } catch (err) {
+            this.log.debug(`update problem`);
+        }
+    }
+
+
+    async create_state() {
+        this.log.debug(`create state`);
+
+        try {
+            for (var i = 0; i < _arrayNum; i++) {
+                const res = await this.creArrayNum(i + 1);
+
+            }
+            this.setState('info.connection', true, true);
+        } catch (err) {
+            this.log.debug(`create state problem`);
+        }
+    }
+
+    async creArrayNum(a) {
+        this.extendObjectAsync(`ArrayNum.${a}`, {
+            type: 'channel',
+            common: {
+                name: `ArrayNum`,
+            },
+            native: {},
+        });
+
+        this.extendObjectAsync(`ArrayNum.${a}.ArrayVoltage`, {
+            type: 'state',
+            common: {
+                name: `ArrayVoltage`,
+                type: 'number',
+                read: true,
+                write: false,
+                def: 0,
+                role: 'info',
+                unit: 'V'
+            },
+            native: {},
+        });
+
+        this.extendObjectAsync(`ArrayNum.${a}.PackVoltage`, {
+            type: 'state',
+            common: {
+                name: `PackVoltage`,
+                type: 'number',
+                read: true,
+                write: false,
+                def: 0,
+                role: 'info',
+                unit: 'V'
+            },
+            native: {},
+        });
+
+        this.extendObjectAsync(`ArrayNum.${a}.Current`, {
+            type: 'state',
+            common: {
+                name: `Current`,
+                type: 'number',
+                read: true,
+                write: false,
+                def: 0,
+                role: 'info',
+                unit: 'A'
+            },
+            native: {},
+        });
+
+        this.extendObjectAsync(`ArrayNum.${a}.SOC`, {
+            type: 'state',
+            common: {
+                name: `SOC`,
+                type: 'number',
+                read: true,
+                write: false,
+                def: 0,
+                role: 'battery.percent',
+                unit: '%'
+            },
+            native: {},
+        });
+        this.extendObjectAsync(`ArrayNum.${a}.SysTemp`, {
+            type: 'state',
+            common: {
+                name: `SysTemp`,
+                type: 'number',
+                read: true,
+                write: false,
+                def: 0,
+                role: 'value.temperature',
+                unit: '°C'
+            },
+            native: {},
+        });
+
+        this.extendObjectAsync(`ArrayNum.${a}.MaxCellVol`, {
+            type: 'state',
+            common: {
+                name: `MaxCellVol`,
+                type: 'number',
+                read: true,
+                write: false,
+                def: 0,
+                role: 'info',
+                unit: 'V'
+            },
+            native: {},
+        });
+        this.extendObjectAsync(`ArrayNum.${a}.MaxCellTemp`, {
+            type: 'state',
+            common: {
+                name: `MaxCellTemp`,
+                type: 'number',
+                read: true,
+                write: false,
+                def: 0,
+                role: 'value.temperature',
+                unit: '°C'
+            },
+            native: {},
+        });
+        this.extendObjectAsync(`ArrayNum.${a}.MinCellTemp`, {
+            type: 'state',
+            common: {
+                name: `MinCellTemp`,
+                type: 'number',
+                read: true,
+                write: false,
+                def: 0,
+                role: 'value.temperature',
+                unit: '°C'
+            },
+            native: {},
+        });
+        this.extendObjectAsync(`ArrayNum.${a}.MaxVolPos`, {
+            type: 'state',
+            common: {
+                name: `MaxVolPos`,
+                type: 'number',
+                read: true,
+                write: false,
+                def: 0,
+                role: 'info'
+            },
+            native: {},
+        });
+        this.extendObjectAsync(`ArrayNum.${a}.MinVolPos`, {
+            type: 'state',
+            common: {
+                name: `MinVolPos`,
+                type: 'number',
+                read: true,
+                write: false,
+                def: 0,
+                role: 'info'
+            },
+            native: {},
+        });
+        this.extendObjectAsync(`ArrayNum.${a}.MaxTempPos`, {
+            type: 'state',
+            common: {
+                name: `MaxTempPos`,
+                type: 'number',
+                read: true,
+                write: false,
+                def: 0,
+                role: 'info'
+            },
+            native: {},
+        });
+        this.extendObjectAsync(`ArrayNum.${a}.MinTempPos`, {
+            type: 'state',
+            common: {
+                name: `MinTempPos`,
+                type: 'number',
+                read: true,
+                write: false,
+                def: 0,
+                role: 'info'
+            },
+            native: {},
+        });
+        this.extendObjectAsync(`ArrayNum.${a}.Power`, {
+            type: 'state',
+            common: {
+                name: `Power`,
+                type: 'number',
+                read: true,
+                write: false,
+                def: 0,
+                role: 'info',
+                unit: 'KW'
+            },
+            native: {},
+        });
+
+        for (var b = 0; b < _batteryNum; b++) {
+            await this.batteryNum(a, b+1);
+
+        }
+
+
+    }
+    async batteryNum(a, b) {
+        this.extendObjectAsync(`ArrayNum.${a}.BattNum`, {
+            type: 'channel',
+            common: {
+                name: `BattNum`,
+            },
+            native: {},
+        });
+        this.extendObjectAsync(`ArrayNum.${a}.BattNum.${b}`, {
+            type: 'channel',
+            common: {
+                name: `BattNum`,
+            },
+            native: {},
+        });
+
+        this.extendObjectAsync(`ArrayNum.${a}.BattNum.${b}.SerialNumber`, {
+            type: 'state',
+            common: {
+                name: `SerialNumber`,
+                type: 'string',
+                read: true,
+                write: false,
+                role: 'info'
+            },
+            native: {},
+        });
+        this.extendObjectAsync(`ArrayNum.${a}.BattNum.${b}.BattVol`, {
+            type: 'state',
+            common: {
+                name: `BattVol`,
+                type: 'number',
+                read: true,
+                write: false,
+                def: 0,
+                role: 'info',
+                unit: 'V'
+            },
+            native: {},
+        });
+
+        for (var cell = 1; cell < 17; cell++) {
+            this.extendObjectAsync(`ArrayNum.${a}.BattNum.${b}.CellVol${cell}`, {
+                type: 'state',
+                common: {
+                    name: `CellVol${cell}`,
+                    type: 'number',
+                    read: true,
+                    write: false,
+                    def: 0,
+                    role: 'info',
+                    unit: 'V'
+                },
+                native: {},
+            });
+        }
+        this.extendObjectAsync(`ArrayNum.${a}.BattNum.${b}.CellVolMax`, {
+            type: 'state',
+            common: {
+                name: `CellVolMax`,
+                type: 'number',
+                read: true,
+                write: false,
+                def: 0,
+                role: 'info',
+                unit: 'V'
+            },
+            native: {},
+        });
+        this.extendObjectAsync(`ArrayNum.${a}.BattNum.${b}.CellVolMin`, {
+            type: 'state',
+            common: {
+                name: `CellVolMin`,
+                type: 'number',
+                read: true,
+                write: false,
+                def: 0,
+                role: 'info',
+                unit: 'V'
+            },
+            native: {},
+        });
+
+        for (var cell = 1; cell < 5; cell++) {
+            this.extendObjectAsync(`ArrayNum.${a}.BattNum.${b}.CellTemp${cell}`, {
+                type: 'state',
+                common: {
+                    name: `CellTemp${cell}`,
+                    type: 'number',
+                    read: true,
+                    write: false,
+                    def: 0,
+                    role: 'value.temperature',
+                    unit: '°C'
+                },
+                native: {},
+            });
+        }
+        this.extendObjectAsync(`ArrayNum.${a}.BattNum.${b}.lastInfoUpdate`, {
+            type: 'state',
+            common: {
+                name: 'Date/Time of last information update',
+                type: 'number',
+                role: 'value.time',
+                read: true,
+                write: false
+            },
+            native: { },
+        });
+
+    }
+    async initialization() {
+        try {
+            if (this.config.ip === undefined ) {
+                this.log.debug(`initialization undefined no ip`);
+                callback();
+            }
+
+            if (this.config.arraynum !== undefined ) {
+                _arrayNum = Number(this.config.arraynum);
+            } else {
+                this.log.debug(`initialization undefined arraynum undefined`);
+                callback();
+            }
+
+            if (this.config.batterynum !== undefined ) {
+                _batteryNum = Number(this.config.batterynum);
+            } else {
+                this.log.debug(`initialization undefined batterynum undefined`);
+                callback();
+            }
+            interval = parseInt(this.config.interval * 1000, 10);
+            if (interval < 60000) {
+                interval = 60000;
+            }
+
+        } catch (error) {
+            this.log.error('other problem');
+        }
+    }
+
+}
+// @ts-ignore parent is a valid property on module
+if (module.parent) {
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new bydbattControll(options);
+} else {
+    // otherwise start the instance directly
+    new bydbattControll();
+}
